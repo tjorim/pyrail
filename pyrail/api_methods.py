f36644baa485feaa6f7cce619a4514265cb6aad3
Jorim Tielemans <tielemans.jorim@gmail.com>
2025-01-03 14:38:08 +0100

    Delete pyrail/api_methods.py

--- pyrail/api_methods.py
+++
@@ -1,7 +0,0 @@
-methods = {
-    'stations': [],
-    'liveboard': ['id', 'station', 'date', 'time', 'arrdep', 'alerts'],
-    'connections': ['from', 'to', 'date', 'time', 'timesel', 'typeOfTransport', 'alerts', 'results'],
-    'vehicle': ['id', 'date', 'alerts'],
-    'disturbances': []
-}
